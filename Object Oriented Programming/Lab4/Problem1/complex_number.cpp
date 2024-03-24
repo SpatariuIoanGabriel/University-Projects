b20af540ce69374f18bb8b3be7a3ce7dfb7eda00
Ioan Gabriel Spatariu <126616594+SpatariuIoanGabriel@users.noreply.github.com>
2024-03-24 23:55:58 +0200

    Delete Object Oriented Programming/Lab4 directory

--- Object Oriented Programming/Lab4/Problem1/complex_number.cpp
+++
@@ -1,8 +0,0 @@
-#include "complex_number.h"
-#include <sstream>
-#include <iomanip>
-#include <sstream>
-#include <iostream>
-using namespace std;
-#define M_PI  3.1415926535f 
- 
--- Object Oriented Programming/Lab4/Problem2/BigInteger.cpp
+++
@@ -1,6 +0,0 @@
-#include "BigInteger.h"
-
-#include <cctype> // for isdigit
-#include <iostream>
-#include <iomanip>
-using namespace std;
